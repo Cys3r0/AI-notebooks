bbc4928d2c50ffdec08df267fea3c04ce813244f
Cys3r0 <cysero.inc@gmail.com>
2026-04-23 22:21:41 +0200

    setup

---
+++ dropout.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+from torch import Tensor
+#TODO read the paper
+
+class DropOut(nn.Module):
+    def __init__():
+        super().__init__()
+        pass
+
+    def forward(self, x: Tensor) -> Tensor:
+        pass
+
+
